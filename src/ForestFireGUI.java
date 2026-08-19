2326d031f35ca55a301e4111b978bf36bde41c82
Matheus Ladeira <mths.ladeira@gmail.com>
2026-08-19 13:32:48 +0000

    added visualization (in the terminal)

--- src/ForestFireGUI.java
+++ src/ForestFireGUI.java
@@ -13,10 +13,37 @@ public class ForestFireGUI {
 
         boolean fireStillBuring = true;
         while (fireStillBuring) {
-            fireStillBuring = forestField.spreadFireStep();
+
             // update GUI according to forestField data
+            printForestField();
             
+            fireStillBuring = forestField.spreadFireStep();
+
             // wait for a while
+            try {
+                Thread.sleep(1000); // 1 second
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
         }
+    }
+
+    private void printForestField() {
+        for (int y = forestField.getH(); y > 0; y--) {
+            for (int x = 1; x <= forestField.getL(); x++) {
+                int state = forestField.getForestFieldSlot(x, y);
+                if (state == 0) {
+                    System.out.print("o");
+                } else if (state == 1) {
+                    System.out.print("ˆ");
+                } else if (state == 2) {
+                    System.out.print(".");
+                } else {
+                    System.out.print("?");
+                }
+            }
+            System.out.println();
+        }
+        System.out.println("-----");
     }
 }
